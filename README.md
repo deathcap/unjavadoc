d45733fb17c946d808b5e6e0e2b5dbca2cf0f474
deathcap <deathcap@gmx.co.uk>
2014-09-05 19:36:33 -0700

    Rename to unjavadoc

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# javadoc2java
+# unjavadoc
 
 ## License
 
--- javadoc2java.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var fs = require('fs');
-
-var fn = '../jd-bukkit/jd.bukkit.org/rb/apidocs/org/bukkit/Achievement.html';
-
-console.log(fs.readFileSync(fn, 'utf8'));
